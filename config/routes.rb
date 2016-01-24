3cb2a4e0c678b7e425f3f7130f114a1831346bc3
Steven Stromberg <gyozaguy@gmail.com>
2016-01-23 21:00:53 -0700

    Adds update action to the users controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json }, path: '/' do # constraints: { subdomain: 'api' }
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
-      resources :users, only: [:show, :create]
+      resources :users, only: [:show, :create, :update]
     end
   end
 end
