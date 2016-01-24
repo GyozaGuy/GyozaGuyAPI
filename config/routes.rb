b128f0a9820b41120f5e85fa9ee30665832c264b
Steven Stromberg <gyozaguy@gmail.com>
2016-01-23 21:24:32 -0700

    Added Subisu-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
+  mount SabisuRails::Engine => "/sabisu_rails"
   devise_for :users
   # API definition
-  namespace :api, defaults: { format: :json }, path: '/' do # constraints: { subdomain: 'api' }
+  namespace :api, defaults: { format: :json }, path: '/api' do # constraints: { subdomain: 'api' }
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
       resources :users, only: [:show, :create, :update, :destroy]
