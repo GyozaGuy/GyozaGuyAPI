8d2b67eb75f6b79e24a76e802b8caf9f63f6eb69
Steven Stromberg <gyozaguy@gmail.com>
2016-02-10 23:24:44 -0700

    Adds search scopes on the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   validates :title, :time, :content, :user_id, presence: true
   # validates :time, :timeliness, presence: true # TODO: maybe use https://github.com/adzap/validates_timeliness
   belongs_to :user
+  scope :filter_by_title, lambda { |keyword| where("lower(title) LIKE ?", "%#{keyword.downcase}%") }
+  scope :before_or_equal_to_time, lambda { |time| where("time <= ?", time) }
+  scope :after_or_equal_to_time, lambda { |time| where("time >= ?", time) }
+  scope :recent, -> { order(:updated_at) }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -18,4 +18,64 @@ describe Post do
   it { should validate_presence_of :content }
   it { should validate_presence_of :user_id }
   it { should belong_to :user }
+
+  describe '.filter_by_title' do
+    before(:each) do
+      @post1 = FactoryGirl.create :post, title: 'Test post 1 of doom'
+      @post2 = FactoryGirl.create :post, title: 'Test post 2'
+      @post3 = FactoryGirl.create :post, title: 'Test post of doom 3'
+      @post4 = FactoryGirl.create :post, title: 'Test post 4'
+    end
+
+    context "when a 'doom' title pattern is sent" do
+      it 'returns the 2 posts matching' do
+        expect(Post.filter_by_title('doom')).to have(2).items
+      end
+
+      it 'returns the posts matching' do
+        expect(Post.filter_by_title('doom').sort).to match_array([@post1, @post3])
+      end
+    end
+  end
+
+  describe '.before_or_equal_to_time' do
+    before(:each) do
+      @post1 = FactoryGirl.create :post, time: Time.now
+      @post2 = FactoryGirl.create :post, time: Time.at(628232400)
+      @post3 = FactoryGirl.create :post, time: Time.now + 1.day
+      @post4 = FactoryGirl.create :post, time: 1.day.ago
+    end
+
+    it 'returns the posts which are before or equal to the time' do
+      expect(Post.before_or_equal_to_time(Time.now).sort).to match_array([@post1, @post2, @post4])
+    end
+  end
+
+  describe '.after_or_equal_to_time' do
+    before(:each) do
+      @post1 = FactoryGirl.create :post, time: Time.now
+      @post2 = FactoryGirl.create :post, time: Time.at(628232400)
+      @post3 = FactoryGirl.create :post, time: Time.now + 1.day
+      @post4 = FactoryGirl.create :post, time: 1.day.ago
+    end
+
+    it 'returns the posts which are after or equal to the time' do
+      expect(Post.after_or_equal_to_time(Time.now).sort).to match_array([@post3]) # The original Time.now used on post1 is no longer current so should not match
+    end
+  end
+
+  describe '.recent' do
+    before(:each) do
+      @post1 = FactoryGirl.create :post, time: Time.now
+      @post2 = FactoryGirl.create :post, time: Time.at(628232400)
+      @post3 = FactoryGirl.create :post, time: Time.now + 1.day
+      @post4 = FactoryGirl.create :post, time: 1.day.ago
+      @post2.touch
+      @post3.touch
+    end
+
+    it 'returns the most updated records' do
+      expect(Post.recent).to match_array([@post3, @post2, @post4, @post1])
+    end
+  end
 end
