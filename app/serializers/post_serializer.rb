3dedeadda502b669df0ad488c0505b1f7f7ef70e
Steven Stromberg <gyozaguy@gmail.com>
2016-02-12 19:27:11 -0700

    Adds caching for the serializers

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,10 @@
 class PostSerializer < ActiveModel::Serializer
+  cached
+  
   attributes :id, :title, :time, :content, :published
   has_one :user
+
+  def cache_key
+    [object, scope]
+  end
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
+  cached
+
   embed :ids
   attributes :id, :email, :created_at, :updated_at, :auth_token
   has_many :posts
+
+  def cache_key
+    [object, scope]
+  end
 end
